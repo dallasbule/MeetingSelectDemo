dbb554b3c785f2ffa514b29cced5dc034d684bbb
siq <2551327011@qq.com>
2017-08-21 15:17:40 +0800

    Initial commit

---
+++ app/src/main/java/example/com/meetingselectdemo/MainActivity.java
@@ -0,0 +1,49 @@
+package example.com.meetingselectdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+
+
+    public MeetingMapView seatTableView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        seatTableView = (MeetingMapView) findViewById(R.id.seatView);
+        seatTableView.setRowAndColumn(9,14);
+        seatTableView.setSeatChecker(new MeetingMapView.SeatChecker() {
+
+            @Override
+            public boolean isValidSeat(int row, int column) {
+                if (row==2||row==1){
+                    return false;
+                }
+                if (row==0&&(column==0||column==1||column==8||column==9)){
+                    return false;
+                }
+                return true;
+            }
+
+            @Override
+            public boolean isSelect(int row, int column) {
+                return false;
+            }
+
+            @Override
+            public void checked(int row, int column) {
+
+            }
+
+            @Override
+            public void unCheck(int row, int column) {
+
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/example/com/meetingselectdemo/MeetingMapView.java
@@ -0,0 +1,488 @@
+package example.com.meetingselectdemo;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by Administrator on 2017/8/10.
+ */
+
+public class MeetingMapView extends View {
+
+    //用户绘制图形的画笔
+    Paint paint = new Paint();
+
+    //用于图形变换
+    Matrix matrix = new Matrix();
+    //用于每个小座位图的变换位移
+    Matrix seatMatrix = new Matrix();
+    //用于桌子的变换位移
+
+
+    /**
+     * 以下均为变量的定义
+     *
+     * @param context
+     */
+
+    //座位的水平间距
+    private int horizontalSpacing;
+    //座位的垂直间距
+    private int vericalSpacing;
+    //行数
+    private int rowNum;
+    //列数
+    private int column;
+    //会议室桌子的宽度
+    private int tableWidth;
+    //会议室桌子的高度
+    private float tableHeight;
+
+
+    //座位已经选中
+    private static final int SEAT_TYPE_SELECTED = 1;
+    private int selectSeatId;
+    //座位可选
+    private static final int SEAT_TYPE_AVAILABLE = 0;
+    private int availableSeatId;
+    //不显示座位
+    private static final int SEAT_NOT_SHOW = 2;
+    //桌子
+    private static final int CHAIR = 3;
+    //可选时座位的图片
+    Bitmap availableSeatBitmap;
+    //选中时座位的图片
+    Bitmap selectSeatBitmap;
+    //桌子的图片
+    Bitmap chairBitmap;
+    //默认的座位图宽度
+    private float defaultImgW = 50;
+    //默认的座位图高度
+    private float defaultImgH = 44;
+    //整个座位图的高度
+    private int totalSeatHeight;
+    //整个座位图的宽度
+    private int totalSeatWidth;
+
+    private SeatChecker seatChecker;
+
+    //Y轴起始坐标
+    private int startY = getHeight() / 7;
+
+
+    public MeetingMapView(Context context) {
+        super(context);
+    }
+
+    public MeetingMapView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public MeetingMapView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+
+    private void init(Context context, AttributeSet attr) {
+        TypedArray typedArray = context.obtainStyledAttributes(attr, R.styleable.SeatTableView);
+        selectSeatId = typedArray.getResourceId(R.styleable.SeatTableView_seat_sold, R.drawable.seat_sold);
+        availableSeatId = typedArray.getResourceId(R.styleable.SeatTableView_seat_available, R.drawable.seat_green);
+        typedArray.recycle();
+
+    }
+
+    //缩放比例
+    float scaleX;
+    float scaleY;
+
+    private void init() {
+        availableSeatBitmap = BitmapFactory.decodeResource(getResources(), availableSeatId);
+        selectSeatBitmap = BitmapFactory.decodeResource(getResources(), selectSeatId);
+
+        horizontalSpacing = dip2Px(getContext(), 10);
+        vericalSpacing = dip2Px(getContext(), 10);
+        tableWidth = dip2Px(getContext(), 80);
+
+        //缩放比列，主要用于资源图片的初始化大小，谨防图片过大导致
+        scaleX = defaultImgW / availableSeatBitmap.getWidth();
+        scaleY = defaultImgH / availableSeatBitmap.getHeight();
+        //计算整个图的高
+        totalSeatWidth = (int) (column * availableSeatBitmap.getWidth() * scaleX + (column - 1) * horizontalSpacing);
+        totalSeatHeight = (int) (rowNum * availableSeatBitmap.getHeight() * scaleY * (rowNum - 1) * vericalSpacing);
+
+        //桌子的高度
+        tableHeight = dip2Px(getContext(), 30);
+
+//        matrix.postTranslate(horizontalSpacing, tableHeight + vericalSpacing * 3);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (rowNum <= 0 || column <= 0) {
+            return;
+        }
+        drawSeat(canvas);
+//        drawTable(canvas);
+    }
+
+    //第一次按下的坐标
+    private int downX;
+    private int downY;
+    //离开的时候坐标
+    private int endX;
+    private int endY;
+
+    //对界面的操控，主要是平移
+    public boolean onTouchEvent(MotionEvent event) {
+        int y = (int) event.getY();
+        int x = (int) event.getX();
+        super.onTouchEvent(event);
+        gestureDetector.onTouchEvent(event);
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downX = x;
+                downY = y;
+                invalidate();
+                break;
+            //判断是为了到达边界不在移动
+            case MotionEvent.ACTION_MOVE:
+                int distanceX = Math.abs(x - downX);
+                int distanceY = Math.abs(y - downY);
+                int translateX = x - endX;
+                int translateY = y - endY;
+                if (getWidth()>totalSeatWidth) {
+                    if (Math.abs(getTranslateX()) < (getWidth() - totalSeatWidth) / 2) {
+                        if (distanceX > 10 || distanceY > 10) {
+                            matrix.postTranslate(translateX, translateY);
+                            invalidate();
+                        }
+                    } else {
+                        if (getTranslateX() < 0) {
+                            if ((distanceX > 10 || distanceY > 10) && translateX > 0) {
+                                matrix.postTranslate(translateX, translateY);
+                                invalidate();
+                            }
+                        } else {
+                            if ((distanceX > 10 || distanceY > 10) && translateX < 0) {
+                                matrix.postTranslate(translateX, translateY);
+                                invalidate();
+                            }
+                        }
+                    }
+                }else {
+                    if (Math.abs(getTranslateX()) < (totalSeatWidth -getWidth()) / 2) {
+                        if (distanceX > 10 || distanceY > 10) {
+                            matrix.postTranslate(translateX, translateY);
+                            invalidate();
+                        }
+                    } else {
+                        if (getTranslateX() < 0) {
+                            if ((distanceX > 10 || distanceY > 10) && translateX > 0) {
+                                matrix.postTranslate(translateX, translateY);
+                                invalidate();
+                            }
+                        } else {
+                            if ((distanceX > 10 || distanceY > 10) && translateX < 0) {
+                                matrix.postTranslate(translateX, translateY);
+                                invalidate();
+                            }
+                        }
+                    }
+
+                }
+                break;
+        }
+        endX = x;
+        endY = y;
+
+        return true;
+    }
+
+//    private void drawTable(Canvas canvas) {
+//        //绘制桌子的paint初始化
+//        paint.setStyle(Paint.Style.FILL);
+//        paint.setColor(Color.GREEN);
+//        paint.setAntiAlias(true);
+//
+//        float centerX = getWidth() / 2 + getTranslateX();
+//        float tableWidth = totalSeatWidth * 0.5f;
+//        if (tableWidth < this.tableWidth) {
+//            tableWidth = this.tableWidth;
+//        }
+//
+//        //开始绘制
+//        Path path = new Path();
+//        path.moveTo(centerX, startY + getTranslateY());
+//        path.lineTo(centerX - tableWidth / 2, startY + getTranslateY());
+//        path.lineTo(centerX - tableWidth / 2, tableHeight + startY + getTranslateY());
+//        path.lineTo(centerX + tableWidth / 2, tableHeight + startY + getTranslateY());
+//        path.lineTo(centerX + tableWidth / 2, startY + getTranslateY());
+//
+//        canvas.drawPath(path, paint);
+//
+//    }
+
+    //绘制座位图
+
+    private void drawSeat(Canvas canvas) {
+        //获取x轴平移距离
+        float translateX = getTranslateX();
+        //获取Y轴平移距离
+        float translateY = getTranslateY();
+
+        for (int i = 0; i < rowNum; i++) {
+            //this.scaleY是座位资源图片的初始化缩放比例
+            float top = i * availableSeatBitmap.getHeight() * this.scaleY + i * vericalSpacing + startY + tableHeight * 2 + translateY;
+
+            float bottom = top + availableSeatBitmap.getHeight() * this.scaleY;
+            //如果平移超过屏幕，则不继续绘制
+            if (bottom < 0 || top > getHeight()) {
+                continue;
+            }
+            for (int j = 0; j < column; j++) {
+                float left = j * availableSeatBitmap.getWidth() * this.scaleX + j * horizontalSpacing + translateX + (getWidth() - totalSeatWidth) / 2;
+                float right = availableSeatBitmap.getWidth() * this.scaleX + left;
+                if (right < 0 || left > getWidth()) {
+                    continue;
+                }
+                //根据行列数搜索座位判定其状态
+                int seatType = getSeatType(i, j);
+                //平移获取新图
+                seatMatrix.setTranslate(left, top);
+                //按照比列缩放初始化每个座位图
+                seatMatrix.postScale(this.scaleX, this.scaleY, left, top);
+                //根据获取的状态来显示
+                switch (seatType) {
+                    case CHAIR:
+                        break;
+                    case SEAT_NOT_SHOW:
+                        break;
+                    case SEAT_TYPE_AVAILABLE:
+                        canvas.drawBitmap(availableSeatBitmap, seatMatrix, paint);
+                        break;
+                    case SEAT_TYPE_SELECTED:
+                        canvas.drawBitmap(selectSeatBitmap, seatMatrix, paint);
+                        break;
+                }
+            }
+
+        }
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+
+    public static int dip2Px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    //此接口用于改变和存储座位状态
+    public interface SeatChecker {
+        /**
+         * 是否可用座位
+         *
+         * @param row
+         * @param column
+         * @return
+         */
+        boolean isValidSeat(int row, int column);
+
+        /**
+         * 是否已经选择
+         *
+         * @param row
+         * @param column
+         * @return
+         */
+        boolean isSelect(int row, int column);
+
+        void checked(int row, int column);
+
+        void unCheck(int row, int column);
+
+    }
+
+    float[] m = new float[9];
+
+    private float getTranslateX() {
+        matrix.getValues(m);
+        return m[Matrix.MTRANS_X];
+    }
+
+    private float getTranslateY() {
+        matrix.getValues(m);
+        return m[Matrix.MTRANS_Y];
+    }
+
+    //设置行数和列数
+    public void setRowAndColumn(int rowNum, int column) {
+        this.rowNum = rowNum;
+        this.column = column;
+        init();
+        invalidate();
+    }
+
+    public void setSeatChecker(SeatChecker seatChecker) {
+        this.seatChecker = seatChecker;
+        invalidate();
+    }
+
+    /***
+     * 以下是点击事件的
+     */
+
+
+    //存储选中的座位
+    ArrayList<Integer> selects = new ArrayList<>();
+
+    private int isHave(Integer seat) {
+        //使用二分查找法查找并返回所在数组位置，判定是否已经被选中
+        return Collections.binarySearch(selects, seat);
+    }
+
+    //为每一个位置分配一个唯一的id用作数组下坐标
+    private int getID(int row, int column) {
+        return row * this.column + (column + 1);
+    }
+
+    //获取是否选取，即该座位是否被选取
+    private int getSeatType(int row, int column) {
+
+        if (isHave(getID(row, column)) >= 0) {
+            return SEAT_TYPE_SELECTED;
+        }
+        if (seatChecker != null) {
+            if (!seatChecker.isValidSeat(row, column)) {
+                return SEAT_NOT_SHOW;
+            }
+            if (seatChecker.isSelect(row, column)) {
+                return SEAT_TYPE_SELECTED;
+            }
+        }
+
+        return SEAT_TYPE_AVAILABLE;
+    }
+
+    private void remove(int index) {
+        selects.remove(index);
+    }
+
+
+    /**
+     * 方法内判断是为了将选中的座位顺序按照数组下标大小排列
+     *
+     * @param row
+     * @param column
+     */
+    private void addChooseSeat(int row, int column) {
+        int id = getID(row, column);
+        for (int i = 0; i < selects.size(); i++) {
+            int item = selects.get(i);
+            if (id < item) {
+                selects.add(i, id);
+                return;
+            }
+        }
+        selects.add(id);
+    }
+
+    GestureDetector gestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            int x = (int) e.getX();
+            int y = (int) e.getY();
+
+            for (int i = 0; i < rowNum; i++) {
+                for (int j = 0; j < column; j++) {
+                    /**获取屏幕每个座位的坐标范围
+                     *
+                     */
+                    int top = (int) (i * availableSeatBitmap.getHeight() * scaleY + i * vericalSpacing + startY + tableHeight * 2 + getTranslateY());
+
+                    int bottom = (int) (top + availableSeatBitmap.getHeight() * scaleY);
+
+
+                    int left = (int) (j * availableSeatBitmap.getWidth() * scaleX + j * horizontalSpacing + getTranslateX()+(getWidth() - totalSeatWidth) / 2);
+                    int right = (int) (availableSeatBitmap.getWidth() * scaleX + left);
+
+                    if (seatChecker != null && seatChecker.isValidSeat(i, j)) {
+                        if (x >= left && x <= right && y >= top && y <= bottom) {
+                            int id = getID(i, j);
+                            int index = isHave(id);
+                            if (index >= 0) {
+                                remove(index);
+                                if (seatChecker != null) {
+                                    seatChecker.unCheck(i, j);
+                                }
+                                invalidate();
+                            } else {
+                                showPeoplDialog(i, j);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return super.onSingleTapConfirmed(e);
+        }
+    });
+
+    /**
+     * 人员列表弹出框
+     */
+    private void showPeoplDialog(final int rowNum, final int column) {
+        new AlertDialog.Builder(getContext())
+                .setTitle("请选择人员")
+                .setSingleChoiceItems(new String[]{"惠磊", "惠子怡", "槽子仪", "曹磊"}, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        addChooseSeat(rowNum, column);
+                        if (seatChecker != null) {
+                            seatChecker.checked(rowNum, column);
+                        }
+                        invalidate();
+                        dialogInterface.dismiss();
+                    }
+                })
+                .setNegativeButton("取消", null)
+                .show();
+    }
+
+    /**
+     * 平移防止越界
+     */
+    private void AutoAdjust() {
+    }
+}
